4113a3c9d5c368459fb99663cd2c48995d513df6
Khanyisa_Faith <khanyisafaithmokgolobotho@gmail.com>
2024-06-24 11:39:22 +0200

    Update user.py

--- routes/user.py
+++ routes/user.py
@@ -2,12 +2,10 @@ from fastapi import APIRouter
 from config.db import conn
 from models.index import users
 from schemas.index import User
+
 user = APIRouter()
 
-@user.get("/")
-async def read_data():
-    return conn.execute(users.select()).fetchall()
-
+ 
 @user.get("/{id}")
 async def read_data(id:int):
     return conn.execute(users.select().where(users.c.id)).fetchall()
@@ -21,17 +19,17 @@ async def write_data(user: User):
     ))
     return conn.execute(users.select()).fetchall()
 
-@user.get("/{id}")
+@user.put("/{id}")
 async def update_data(id:int,user:User):
-    conn.execute(users.update(
+    conn.execute(users.update().values(
         name=user.name,
         email=user.email,
         password=user.password
-    ))
+    ).where(users.c.id))
     return conn.execute(users.select()).fetchall()
 
-@user.get("/")
-async def delete_data():
+@user.delete("/{id}")
+async def delete_data(id:int):
     conn.execute(users.delete().where(users.c.id))
     return conn.execute(users.select()).fetchall()
  
